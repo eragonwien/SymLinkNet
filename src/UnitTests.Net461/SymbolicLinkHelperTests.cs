170c8634f1bd0f144dc829a8afb8f0fbab7327b2
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2021-10-17 10:33:36 +0200

    Renames solution and adjusts namespaces

---
+++ src/UnitTests.Net461/SymbolicLinkHelperTests.cs
@@ -0,0 +1,45 @@
+﻿using HoangSonNguyen.IO.Helpers;
+using NUnit.Framework;
+
+namespace UnitTests.Net461
+{
+    [TestFixture]
+    public class SymbolicLinkHelperTests
+    {
+        [Test]
+        public void DirectoryCreateSymbolicLinkTest()
+        {
+            SymbolicLinkHelper.Directory.CreateSymbolicLink("", "");
+        }
+
+        [Test]
+        public void DirectoryGetSymbolicLinkTargetPathTest()
+        {
+            SymbolicLinkHelper.Directory.GetSymbolicLinkTargetPath("");
+        }
+
+        [Test]
+        public void DirectoryIsSymbolicLinkTest()
+        {
+            SymbolicLinkHelper.Directory.IsSymbolicLink("");
+        }
+
+        [Test]
+        public void FileCreateSymbolicLinkTest()
+        {
+            SymbolicLinkHelper.File.CreateSymbolicLink("", "");
+        }
+
+        [Test]
+        public void FileGetSymbolicLinkTargetPathTest()
+        {
+            SymbolicLinkHelper.File.GetSymbolicLinkTargetPath("");
+        }
+
+        [Test]
+        public void FileIsSymbolicLinkTest()
+        {
+            SymbolicLinkHelper.File.IsSymbolicLink("");
+        }
+    }
+}
---
+++ src/UnitTests.NetCore21/SymbolicLinkHelperTests.cs
@@ -0,0 +1,45 @@
+using HoangSonNguyen.IO.Helpers;
+using NUnit.Framework;
+
+namespace UnitTests.NetCore21
+{
+    [TestFixture]
+    public class SymbolicLinkHelperTests
+    {
+        [Test]
+        public void CreateSymbolicLinkTest()
+        {
+            SymbolicLinkHelper.Directory.CreateSymbolicLink("", "");
+        }
+
+        [Test]
+        public void GetSymbolicLinkTargetPathTest()
+        {
+            SymbolicLinkHelper.Directory.GetSymbolicLinkTargetPath("");
+        }
+
+        [Test]
+        public void IsSymbolicLinkTest()
+        {
+            SymbolicLinkHelper.Directory.IsSymbolicLink("");
+        }
+
+        [Test]
+        public void FileCreateSymbolicLinkTest()
+        {
+            SymbolicLinkHelper.File.CreateSymbolicLink("", "");
+        }
+
+        [Test]
+        public void FileGetSymbolicLinkTargetPathTest()
+        {
+            SymbolicLinkHelper.File.GetSymbolicLinkTargetPath("");
+        }
+
+        [Test]
+        public void FileIsSymbolicLinkTest()
+        {
+            SymbolicLinkHelper.File.IsSymbolicLink("");
+        }
+    }
+}
